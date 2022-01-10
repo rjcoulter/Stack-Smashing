4f78c7e050082ce88eaf071798971e06c2d7715f
Ryan Coulter <ryancoulter22@gmail.com>
2022-01-10 11:51:31 -0500

    Add section on BO like exploitation

--- format-string-vulns/example1.c
+++ format-string-vulns/example1.c
@@ -1,5 +1,5 @@
 /**
- * Bad usage of a format function
+ * Wrong usage of a format function
  */
 int func1(char *user) { 
   printf(user);
@@ -10,4 +10,20 @@ int func1(char *user) {
  */
 int func2(char *user) {
   printf("%s", user);
+}
+
+void main() {
+  // Situation one
+  char tmpbuf[512];
+
+  snprintf(tmpbuf, sizeof (tmpbuf), "foo: %s", user);
+  tmpbuf[sizeof (tmpbuf) - 1] = '\0';
+  syslog(LOG_NOTICE, tmpbuf);
+
+  // BO like exploit
+  char outbuf[512];
+  char buffer[512];
+  sprintf(buffer, "ERR Wrong command %400s", user);
+  sprintf(outbuf, buffer);
+
 }
